29a1b1af3a54b25d70ea37db4b8f43cfcfafa2c8
haki11 <haki_@hotmail.com>
2020-07-05 12:43:11 -0400

    Roll Dice JQuery
    initial commit

---
+++ js/main.mjs
@@ -0,0 +1,87 @@
+import { Game } from "./modules/game.mjs"
+
+
+const maxNumberOfDice = 1; // the max number in the select (can be changed here)
+let game = new Game(); // the game object
+game.rollDice();
+
+let numberOfDice = 1; //game.numberOfDice; // the current number of dice
+console.log(game);
+
+let $select; // select tag to input the number of dice
+let $dice; // tag to contain the image tags
+let $total; // tag for the displayed number
+
+// preload dice image files to reduce flickering
+let images = [];
+for (let i = 1; i <= 1; i++) {
+    let image = new Image();
+    image.src = dieImageSrc(0);
+    images.push(image);
+}
+
+$(document).ready(function(){
+    $select = $("select");
+    $dice = $("#dice");
+    $total = $("#total");
+    insertSelectOptions();
+    insertImages();
+    updateTotal();
+    handleSelectionChanges();
+    handleRollButtonClicks();
+});
+
+function dieImageSrc(side) {
+    return `images/dice/side_${side}.png`;
+}
+
+function insertImages() {
+    for (let die of game.dice) {
+        let side = die.side;
+        let src = dieImageSrc(side);
+        $dice.append(`<img src="${src}" alt="side ${side}">`);
+    }
+}
+
+function insertSelectOptions() {
+    for (let i = 1; i <= maxNumberOfDice; i++) {
+        $select.append(`<option value="${i}" ${(i === numberOfDice)?'selected':''}>${i}</option>`);
+    }
+}
+
+function handleSelectionChanges() {
+    $select.change(function () {
+        console.log("the change event handler is called");
+        let count = parseInt($select.val());
+        if (count !== numberOfDice) {
+            numberOfDice = count;
+            game = new Game(count);
+            game.rollDice();
+            $dice.html("");
+            insertImages();
+            updateTotal();
+            saveSides(game);
+        }
+    });
+}
+
+function updateTotal() {
+    $total.text(game.total);
+}
+
+function handleRollButtonClicks() {
+    $("#roll_button").click(function () {
+        console.log("the click event handler is called");
+        game.rollDice();
+        $dice.find("img").each(function(index){
+            let side = game.dice[index].side;
+            let src = dieImageSrc(side);
+            $(this).attr("src", src).attr("alt", `side ${side}`);
+        });
+        updateTotal();
+        saveSides(game);
+    });
+}
+
+
+
---
+++ js/modules/die.mjs
@@ -0,0 +1,23 @@
+class Die {
+    #n; // it is private
+    constructor(side= 1){
+        console.log("constructor() is called");
+        this.side = side;
+    }
+    rollDie() {
+        console.log("rollDie() is called");
+        this.side = Math.floor(1 + 6 * Math.random());
+    }
+    get side(){
+        console.log("get side() is called");
+        return this.#n;
+    }
+    set side(side){
+        console.log("set side() is called");
+        console.log(`side = ${side}`);
+        console.assert(side > 0, "side must be > 0");
+        console.assert(side <= 6, "side must be <= 6");
+        this.#n = side;
+    }
+}
+export { Die }
